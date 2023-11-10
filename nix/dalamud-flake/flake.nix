664645ef76192468822e0e7e069ba0c6647bda00
GrittyFrog <grittyfrog@fastmail.com>
2023-11-11 10:55:25 +1100

    Update Dalamud flake to have scripts available on path
    Now when using the flake you can just type `dalamud-linux-build` etc...

--- nix/dalamud-flake/flake.nix
+++ nix/dalamud-flake/flake.nix
@@ -3,13 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    dalamud-linux-scripts.url = "github:grittyfrog/DalamudLinuxScripts";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, dalamud-linux-scripts }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          overlays = [ dalamud-linux-scripts.overlay ];
+          inherit system;
+        };
       });
     in
     {
@@ -18,6 +22,7 @@
           packages = [
             pkgs.omnisharp-roslyn
             pkgs.dotnet-sdk_7
+            pkgs.dalamud-linux-scripts
           ];
 
           DOTNET_ROOT=pkgs.dotnet-sdk_7;
